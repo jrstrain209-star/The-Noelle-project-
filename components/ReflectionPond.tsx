11ae3e5020da834acb0cf3ff3e8819a7d0b4b49b
jrstrain209-star <jrstrain209@gmail.com>
2026-06-09 10:45:13 -0700

    Add ReflectionPond component with styled divs

--- components/ReflectionPond.tsx
+++ components/ReflectionPond.tsx
@@ -1,1 +1,8 @@
-
+export default function ReflectionPond() {
+  return (
+    <div className="absolute bottom-8 left-1/2 h-24 w-[420px] -translate-x-1/2 rounded-[50%] bg-cyan-300/20 shadow-[0_0_60px_rgba(103,232,249,0.25)] backdrop-blur-sm">
+      <div className="absolute left-16 top-8 h-2 w-28 rounded-full bg-white/20" />
+      <div className="absolute right-14 top-12 h-2 w-36 rounded-full bg-white/10" />
+    </div>
+  );
+}
