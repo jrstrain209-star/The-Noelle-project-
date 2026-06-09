9395e4750f56ec42bc0a1c5ad33827fcb9e200e4
jrstrain209-star <jrstrain209@gmail.com>
2026-06-09 10:46:44 -0700

    Add GardenPath component with styled divs

--- components/GardenPath.tsx
+++ components/GardenPath.tsx
@@ -1,1 +1,11 @@
+export default function GardenPath() {
+  return (
+    <>
+      <div className="absolute bottom-[-80px] left-1/2 h-[620px] w-[360px] -translate-x-1/2 rounded-t-full bg-gradient-to-b from-stone-500 via-stone-700 to-stone-950 shadow-2xl" />
 
+      <div className="absolute bottom-32 left-1/2 h-8 w-20 -translate-x-1/2 rounded-full bg-stone-400/70 shadow-lg" />
+      <div className="absolute bottom-52 left-[47%] h-7 w-16 rounded-full bg-stone-500/70 shadow-lg" />
+      <div className="absolute bottom-72 left-[51%] h-6 w-14 rounded-full bg-stone-500/60 shadow-lg" />
+    </>
+  );
+}
